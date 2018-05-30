c64e38dc89f1cc9a8a55f15b4fbf3144c9912069
William Connelly <williamconnelly.dev@gmail.com>
2018-05-29 23:15:24 -0700

    Completed Assignment

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Takes data from the input field and appends it to a waiting div 
+var addListItem = function(e) {
+	e.preventDefault();
+	var formData = $("form").serializeArray();
+	$(".to-do-list").append("<li><input type='checkbox'>" + " " + formData[0].value + "</li>");
+	$("input")[0].value = "";
+	$(".list-input").focus();
+};
+    
+// Adds event listeners to the submit button for input and to the list items for deletion
+$(document).ready(function() {
+	$("form").submit(addListItem);
+	$(".to-do-list").on("click", "li", function() {
+		$(this).remove();
+		$(".list-input").focus();
+	})
+});
