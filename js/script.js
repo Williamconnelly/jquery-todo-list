17d03708e30f342d18a2d0e947473ae9cf03dd38
William Connelly <williamconnelly.dev@gmail.com>
2018-05-30 08:51:02 -0700

    Fixed click interaction

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@ var addListItem = function(e) {
 // Adds event listeners to the submit button for input and to the list items for deletion
 $(document).ready(function() {
 	$("form").submit(addListItem);
-	$(".to-do-list").on("click", "li", function() {
-		$(this).remove();
+	$(".to-do-list").on("click", "input", function() {
+		$(this).parent().remove();
 		$(".list-input").focus();
 	})
 });
